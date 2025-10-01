8f2e716636562108ddb79f6f7a35dc01b685a3c2
Ravi Suthar <ravisuthar1711@gmail.com>
2025-10-01 22:57:30 +0530

    Added Restaurant Summary

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { React, Fragment } from 'react'
 import './App.css'
 import Header from './components/Layout/Header'
+import MealsSummary from './components/Meals/MealsSummary'
 
 function App() {
 
   return (
     <Fragment>
       <Header />
+      <MealsSummary />
     </Fragment>
   )
 }
