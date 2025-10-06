8301a57421e1bcb65a6d006a5cc707f4f77ba9f2
Ravi Suthar <ravisuthar1711@gmail.com>
2025-10-06 13:06:26 +0530

    Meals card and list rendered

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import classes from './AvailableMeals.module.css';
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_DISHES = [
   {
@@ -35,14 +37,14 @@ const DUMMY_DISHES = [
 ];
 
 const AvailableMeals = () => {
-  const mealList = DUMMY_DISHES.map((meal) => {
-    return <li key={meal.id}>{meal.name}</li>;
-  });
+  const mealList = DUMMY_DISHES.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>);
 
   return (
     <>
       <section className={classes.meals}>
-        <ul>{mealList}</ul>
+        <Card>
+          <ul>{mealList}</ul>
+        </Card>
       </section>
     </>
   );
